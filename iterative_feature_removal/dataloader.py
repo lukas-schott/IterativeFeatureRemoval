63d5e5d1d8d9adf84a7758cf086e22baaa952926
lukas-schott <lukas.schott@gmail.com>
2019-11-20 11:14:31 +0100

    added cifar and mnist c

--- iterative_feature_removal/dataloader.py
+++ iterative_feature_removal/dataloader.py
@@ -4,6 +4,7 @@ import torch
 from torch.utils import data
 import numpy as np
 from iterative_feature_removal import attacks as att
+import torchvision.transforms as transforms
 
 
 class FloatTensorDataset(data.Dataset):
@@ -19,26 +20,41 @@ class FloatTensorDataset(data.Dataset):
 
 
 def get_data_loader(config):
-    mnist_train = datasets.MNIST(config.proj_dir + '/data/', train=True, download=True)
-    mnist_test = datasets.MNIST(config.proj_dir + '/data/', train=False)
-    mnist_test_clean = datasets.MNIST('./data/', train=False)
-    dataset_train = FloatTensorDataset(mnist_train.data.type(torch.float32) / 255., mnist_train.targets)
-    dataset_test = FloatTensorDataset(mnist_test.data.type(torch.float32) / 255., mnist_test.targets)
-    dataset_test_clean = FloatTensorDataset(mnist_test_clean.data.type(torch.float32) / 255., mnist_test_clean.targets)
+    if config.dataset == 'MNIST':
+        mnist_train = datasets.MNIST(config.proj_dir + '/data/', train=True, download=True)
+        mnist_test = datasets.MNIST(config.proj_dir + '/data/', train=False)
+        mnist_test_clean = datasets.MNIST('./data/', train=False)
+        dataset_train = FloatTensorDataset(mnist_train.data.type(torch.float32)[:, None] / 255., mnist_train.targets)
+        dataset_test = FloatTensorDataset(mnist_test.data.type(torch.float32)[:, None] / 255., mnist_test.targets)
+        dataset_test_clean = FloatTensorDataset(mnist_test_clean.data.type(torch.float32)[:, None] / 255.,
+                                                mnist_test_clean.targets)
+
+    elif config.dataset == 'CIFAR10' or config.dataset == 'greyscale_CIFAR10':
+        # transforms
+        transform_train = [transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor()]
+        transform_test = [transforms.ToTensor(), RGB2Greyscale()]
+        if config.dataset == 'greyscale_CIFAR10':
+            transform_train.append(RGB2Greyscale())
+            transform_test.append(RGB2Greyscale())
+        transform_train = transforms.Compose(transform_train)
+        transform_test = transforms.Compose(transform_test)
+
+        dataset_train = datasets.CIFAR10(config.proj_dir + '/data/', train=True, download=True, transform=transform_train)
+        dataset_test = datasets.CIFAR10(config.proj_dir + '/data/', train=False, transform=transform_test)
+        dataset_test_clean = datasets.CIFAR10(config.proj_dir + '/data/', train=False, transform=transform_test)
 
     end = config.end
-    dataset_train.data = dataset_train.data[:end, None]
+    dataset_train.data = dataset_train.data[:end]
     dataset_train.targets = dataset_train.targets[:end]
-    dataset_test.data = dataset_test.data[:end, None]
+    dataset_test.data = dataset_test.data[:end]
     dataset_test.targets = dataset_test.targets[:end]
-    dataset_test_clean.data = dataset_test_clean.data[:end, None]
+    dataset_test_clean.data = dataset_test_clean.data[:end]
     dataset_test_clean.targets = dataset_test_clean.targets[:end]
 
     if config.dataset_modification != 'None':
-        dataset_train, dataset_test, dataset_test_clean = \
-            create_pixel_indicator(dataset_train, dataset_test, dataset_test_clean, config=config)
+        dataset_train, dataset_test =  create_pixel_indicator(dataset_train, dataset_test, config=config)
 
-    print('dataset', torch.min(dataset_train.data), torch.max(dataset_train.data))
     data_loader_train = data.DataLoader(dataset_train, batch_size=config.batch_size, shuffle=True)
     data_loader_test = data.DataLoader(dataset_test, batch_size=config.batch_size, shuffle=False)
     data_loader_test_clean = data.DataLoader(dataset_test_clean, batch_size=config.batch_size, shuffle=False)
@@ -47,13 +63,51 @@ def get_data_loader(config):
     return data_loaders
 
 
+def get_mnist_c(config):
+    n_worker = 4
+    data_loaders_names = \
+        {'Brightness': 'brightness',
+         'Canny Edges': 'canny_edges',
+         'Dotted Line': 'dotted_line',
+         'Fog': 'fog',
+         'Glass Blur': 'glass_blur',
+         'Identity': 'identity',
+         'Impulse Noise': 'impulse_noise',
+         'Motion Blur': 'motion_blur',
+         'Rotate': 'rotate',
+         'Scale': 'scale',
+         'Shear': 'shear',
+         'Shot Noise': 'shot_noise',
+         'Spatter': 'spatter',
+         'Stripe': 'stripe',
+         'Translate': 'translate',
+         'Zigzag': 'zigzag'}
+    data_loaders = {}
+    for name, path in data_loaders_names.items():
+        data_loaders[name] = {}
+
+        test_images = np.load(config.proj_dir + '/data/mnist_c/' + path + '/test_images.npy')
+        test_labels = np.load(config.proj_dir + '/data/mnist_c/' + path + '/test_labels.npy')
+
+        test_images = test_images.squeeze(-1)
+        test_images = np.expand_dims(test_images, axis=1)
+
+        tensor_x = torch.from_numpy(test_images)  # transform to torch tensors
+        tensor_y = torch.from_numpy(test_labels)
+
+        custom_dataset = data.TensorDataset(tensor_x, tensor_y)  # create your datset
+        data_loaders[name] = data.DataLoader(custom_dataset, batch_size=config.batch_size_test,
+                                             num_workers=n_worker)
+    return data_loaders
+
+
 def create_pixel_indicator(*datasets, config=None):
     print(f'using dataset: ', config.dataset_modification)
     for dataset in datasets:
-        # all_coords = np.array([[4, 4], [13, 4], [23, 4], [4, 13], [13, 13], [23, 13], [4, 23], [13, 23],
-        #                    [23, 23], [20, 20]])
-        all_coords = np.array([[12, 9], [14, 10], [12, 11], [14, 12], [12, 13], [14, 14], [12, 15], [14, 16],
-                           [12, 17], [14, 18]])
+        all_coords = np.array([[4, 4], [13, 4], [23, 4], [4, 13], [13, 13], [23, 13], [4, 23], [13, 23],
+                           [23, 23], [20, 20]])
+        # all_coords = np.array([[12, 9], [14, 10], [12, 11], [14, 12], [12, 13], [14, 14], [12, 15], [14, 16],
+        #                    [12, 17], [14, 18]])
         for i, coords in zip(range(10), all_coords):
             dataset_copy = dataset.data[dataset.targets == i]  # somehow not possible in place
             if config.dataset_modification == 'single_feat':
@@ -63,7 +117,8 @@ def create_pixel_indicator(*datasets, config=None):
                 # dataset_copy[:, 0, coords[0]+1, coords[1]+1] = 1
                 # dataset_copy[:, 0, coords[0]+2, coords[1]+2] = 1
             elif config.dataset_modification == 'shift_mnist':
-                dataset_copy[:, 0, i, 0] = 1
+                print('shift mnists', i)
+                dataset_copy[:, 0, i*2+4, 4] = 1
             elif config.dataset_modification == 'double_feat':
                 dataset_copy[:] = 0
                 dataset_copy[:, 0, i, 0] = 1
@@ -112,3 +167,30 @@ def copy_data_loader(data_loader):
     dset_new.targets =  data_loader.dataset.targets.clone()
     new_data_loader = data.DataLoader(dset_new, batch_size=data_loader.batch_size, shuffle=False)
     return new_data_loader
+
+
+def grayscale(data, dtype='float32'):
+    # luma coding weighted average in video systems
+    r, g, b = np.asarray(.333, dtype=dtype), np.asarray(.333, dtype=dtype), np.asarray(.333, dtype=dtype)
+    rst = r * data[:, :, :, 0] + g * data[:, :, :, 1] + b * data[:, :, :, 2]
+    # add channel dimension
+    rst = np.expand_dims(rst, axis=3)
+    return rst
+
+
+class RGB2Greyscale(object):
+    """Horizontally flip the given PIL Image randomly with a given probability.
+
+    Args:
+        p (float): probability of the image being flipped. Default value is 0.5
+    """
+
+    def __init__(self, factors=(0.333, 0.333, 0.333)):
+        self.factors = factors
+
+    def __call__(self, img):
+        grey_img = self.factors[0] * img[0] + self.factors[1] * img[1] + self.factors[2] * img[2]
+        return grey_img[None]
+
+    def __repr__(self):
+        return self.__class__.__name__
